3839699ee2e772dc3de752a79b0ce644f6fe9dca
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-24 10:40:47 +0330

    feat: :sparkles: ADD NAVBAR TO ALL COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Navbar from "./components/common/layout/Navbar";
 import RoutesList from "./components/routes/RoutesList";
 import RouteWrapper from "./components/routes/RouteWrapper";
 import AuthenticationProvider from "./context/AuthenticationProvider";
@@ -6,6 +7,7 @@ function App() {
   return (
     <AuthenticationProvider>
       <RouteWrapper>
+        <Navbar />
         <RoutesList />
       </RouteWrapper>
     </AuthenticationProvider>
